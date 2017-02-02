470c80e0772fa4ad073f0c939068e40ea69dfc21
jie1311 <tommy911113@gmail.com>
2017-02-02 14:18:29 +1100

    On going process of learning
    However, more knowledge is needed for this project.

---
+++ jdbc/src/main/java/src/main/java/hello/JdbcApplication.java
@@ -0,0 +1,12 @@
+package src.main.java.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcApplication.class, args);
+	}
+}
