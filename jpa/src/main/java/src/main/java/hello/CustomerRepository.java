a30f42dbc7538a69066e4b1156bea295e1504b2c
jie1311 <tommy911113@gmail.com>
2017-02-04 00:17:00 +1100

    Accessing Data with JPA
    An application that stores Customer POJOs in a memory-based database.

---
+++ jpa/src/main/java/src/main/java/hello/CustomerRepository.java
@@ -0,0 +1,10 @@
+package src.main.java.hello;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+}
