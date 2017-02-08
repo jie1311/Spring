5808b4a451a20892bfe7b3817d5e225f5e052b7d
jie1311 <tommy911113@gmail.com>
2017-02-08 22:53:52 +1100

    bug fixed

--- secure/src/main/java/com/example/WebSecurityConfig.java
+++ secure/src/main/java/com/example/WebSecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class  WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
